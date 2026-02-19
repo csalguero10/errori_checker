92e4fb0f995906036dca51ff539454766b118dee
Catalina Salguero <c.salguero.p18@gmail.com>
2026-02-19 11:53:18 +0100

    vite fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// 🔴 IMPORTANTE: cambia "errori-checker" por el nombre exacto de tu repositorio en GitHub
 export default defineConfig({
   plugins: [react()],
-  base: '/errori-checker/',
+  base: '/errori_checker/',
 })
